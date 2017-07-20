d53c57a1dae792dd56d9de0091d4a755c663c793
Prabhat Sharma <hi.prabhat@gmail.com>
2017-07-20 14:45:51 -0700

    Added openGranteeURI validation

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import boto3
-import json
 
 s3 = boto3.resource('s3')
-
 s3Client = boto3.client('s3')
 
 openGranteeURI = 'http://acs.amazonaws.com/groups/global/AllUsers'
@@ -11,13 +9,12 @@ print('This script lists world readable S3 buckets that you own:')
 for bucket in s3.buckets.all():
 
     bucketAcl = s3Client.get_bucket_acl(Bucket=bucket.name)
-
     items = bucketAcl['Grants']
 
     for item in items:
 
         openPermissions = item['Permission'] == 'READ' or item['Permission'] == 'FULL_CONTROL'
 
-        if item['Grantee']['Type'] == "Group" and openPermissions:
+        if item['Grantee']['Type'] == "Group" and item['Grantee']['URI'] == openGranteeURI and openPermissions:
             print('Bucket: ', bucket.name)
             print('World Permissions: ',  item['Permission'])
