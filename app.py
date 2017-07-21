67e5c9c7b0ae43942491f4df96bac19a8a3f3d00
Prabhat Sharma <hi.prabhat@gmail.com>
2017-07-21 13:10:36 -0700

    Testing bucket policy

--- app.py
+++ app.py
@@ -9,13 +9,24 @@ openGranteeURI = 'http://acs.amazonaws.com/groups/global/AllUsers'
 
 print('This script lists world readable S3 buckets that you own:')
 for bucket in s3.buckets.all():
+    
+    # bucketPolicy = s3.BucketPolicy(bucket.name).policy 
+    try:
+        bucketPolicy = s3Client.get_bucket_policy(Bucket = bucket.name)
+        for policy in bucketPolicy['Statement']:
+            if policy['Principal'] == "*" and policy['Effect'] == "Allow":
+                print("Resource: ", policy['Resource'])
+                print("Action: ", policy['Action'])
+        print(bucketPolicy)
+    except:
+        bucketPolicy = "No bucket policy"
 
     bucketAcl = s3Client.get_bucket_acl(Bucket=bucket.name)
     grants = bucketAcl['Grants']
 
     for grant in grants:
 
-        openPermissions = grant['Permission'] in ['READ', 'WRITE', 'FULL_CONTROL']
+        openPermissions = grant['Permission'] in ['READ', 'WRITE', 'READ_ACP', 'WRITE_ACP', 'FULL_CONTROL']
 
         if grant['Grantee']['Type'] == "Group" and grant['Grantee']['URI'] == openGranteeURI and openPermissions:
             print('Bucket: ', bucket.name)
