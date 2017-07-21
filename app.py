a299691838bf2dbdb189ca406a0f7f40981205f9
Prabhat Sharma <hi.prabhat@gmail.com>
2017-07-21 13:53:56 -0700

    added support for bucket policies

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import boto3
 import time
+import json
 
 startTime = time.time()
 s3 = boto3.resource('s3')
@@ -10,26 +11,27 @@ openGranteeURI = 'http://acs.amazonaws.com/groups/global/AllUsers'
 print('This script lists world readable S3 buckets that you own:')
 for bucket in s3.buckets.all():
     
-    # bucketPolicy = s3.BucketPolicy(bucket.name).policy 
+    # Check for bucket policies
     try:
         bucketPolicy = s3Client.get_bucket_policy(Bucket = bucket.name)
-        for policy in bucketPolicy['Statement']:
-            if policy['Principal'] == "*" and policy['Effect'] == "Allow":
-                print("Resource: ", policy['Resource'])
-                print("Action: ", policy['Action'])
-        print(bucketPolicy)
+        Statements = json.loads(bucketPolicy['Policy'])['Statement']
+        for Statement in Statements:
+            print('Policy for Bucket: ', bucket.name)
+            if Statement['Principal'] == "*" and Statement['Effect'] == "Allow":
+                print("Policy: Resource: ", Statement['Resource'])
+                print("Policy: Action: ", Statement['Action'])
     except:
         bucketPolicy = "No bucket policy"
 
+    # Check for Bucket ACLs
     bucketAcl = s3Client.get_bucket_acl(Bucket=bucket.name)
     grants = bucketAcl['Grants']
 
     for grant in grants:
-
         openPermissions = grant['Permission'] in ['READ', 'WRITE', 'READ_ACP', 'WRITE_ACP', 'FULL_CONTROL']
 
         if grant['Grantee']['Type'] == "Group" and grant['Grantee']['URI'] == openGranteeURI and openPermissions:
-            print('Bucket: ', bucket.name)
+            print('ACL for Bucket: ', bucket.name)
             print('World Permissions: ',  grant['Permission'])
 
 print('open buckets found in: ', round(time.time() - startTime, 2), ' seconds')
